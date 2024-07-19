d91d253c4d4716bfde2fcdcfef64cb0a81c6a7bb
iNa. <aljard@naver.com>
2024-07-19 22:07:12 +0900

    240719_shopping mall project (header)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,17 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <header>
-      <Link to='/'>
+    <header className='flex justify-between border-b border-gray-300 p-4'>
+      <Link to='/' className='flex items-center text-4xl'>
         <FiShoppingBag />
         <h1>Shoesy</h1>
       </Link>
-      <nav>
+      <nav className='flex items-center gap-4 font-semibold'>
         <Link to='/products'>Products</Link>
         <Link to='/carts'>Carts</Link>
-        <Link to='/products/new'><BsFillPencilFill /></Link>
+        <Link to='/products/new' className='text-2xl'>
+          <BsFillPencilFill />
+        </Link>
         <button>Login</button>
       </nav>
     </header>
