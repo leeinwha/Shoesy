655c1c2c47eae0d5d9f1401d95bf1f0f902a48e5
iNa. <aljard@naver.com>
2024-07-20 23:17:53 +0900

    240720_shopping mall project (login,logout)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BsFillPencilFill } from 'react-icons/bs';
 import { FiShoppingBag } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { login, logout, onUserStateChange } from '../api/firevase';
 
 export default function Navbar() {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    onUserStateChange((user) => {
+      console.log(user);
+      setUser(user);
+    });
+  }, []);
+  const handleLogin = () => {
+    login().then(setUser);
+  }
+  const handleLogout = () => {
+    logout().then(setUser);
+  }
   return (
     <header className='flex justify-between border-b border-gray-300 p-4'>
       <Link to='/' className='flex items-center text-4xl'>
@@ -16,7 +30,8 @@ export default function Navbar() {
         <Link to='/products/new' className='text-2xl'>
           <BsFillPencilFill />
         </Link>
-        <button>Login</button>
+        {!user && <button onClick={handleLogin}>Login</button>}
+        {user && <button onClick={handleLogout}>Logout</button>}
       </nav>
     </header>
   )
