d32ba14a59d1941b9ac236f504b834438e7d687c
iNa. <aljard@naver.com>
2024-07-22 15:26:23 +0900

    240722_shopping mall project (url protect)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,11 @@ import { Link } from 'react-router-dom';
 import { login, logout, onUserStateChange } from '../api/firevase';
 import User from './User';
 import Button from './ui/Button';
+import { useAuthContext } from './context/AuthContext';
 
 export default function Navbar() {
-  const [user, setUser] = useState();
-
-  useEffect(() => {
-    onUserStateChange((user) => {
-      console.log(user);
-      setUser(user);
-    });
-  }, []);
+  
+  const { user, login, logout } = useAuthContext();
 
   return (
     <header className='flex justify-between border-b border-gray-300 p-4'>
@@ -24,7 +19,7 @@ export default function Navbar() {
       </Link>
       <nav className='flex items-center gap-4 font-semibold'>
         <Link to='/products'>Products</Link>
-        <Link to='/carts'>Carts</Link>
+        {user && <Link to='/carts'>Carts</Link>}
         {user && user.isAdmin &&(
           <Link to='/products/new' className='text-2xl'>
             <BsFillPencilFill />
